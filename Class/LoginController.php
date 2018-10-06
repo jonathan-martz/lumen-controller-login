03184f69634408d5652a6c94db55306f93462ba5
Jonathan Martz <git@jmartz.de>
2018-10-06 19:36:10 +0200

    update php version, finish login controller

--- Class/LoginController.php
+++ Class/LoginController.php
@@ -10,17 +10,11 @@
 
 	class LoginController extends BaseController
 	{
-
-		public $result =  [];
-		public $request =  [];
-
-
 		/**
 		 * @param  Request  $request
 		 * @return Response
 		 */
-		public function user(Request $request)
-		{
+		public function user(Request $request){
 			$validation = $this->validate($request, [
 				'username' => 'required',
 				'password' => 'required|min:8'
@@ -28,45 +22,97 @@
 
 			$user = DB::connection('mysql.read')
 					  ->table('users')
-					  ->where('username','=',$request->input('username'))
-					  ->where('username_hash','=',sha1($request->input('username')))
+					  ->where('username', '=', $request->input('username'))
+					  ->where('username_hash', '=', sha1($request->input('username')))
 					  ->first();
 
+			$trys = DB::connection('mysql.read')
+					  ->table('login_try')
+					  ->where('username', '=', $request->input('username'))
+					  ->where('username_hash', '=', sha1($request->input('username')))
+					  ->whereNotIn('status' , ['success'])
+					  ->count();
 
-			if($user !== NULL){
-				if (Hash::check($request->input('password'), $user->password))
-				{
-					$token = bin2hex(openssl_random_pseudo_bytes(64));
+			if($trys < 10){
+				if($user !== NULL){
+					if (Hash::check($request->input('password'), $user->password))
+					{
+						$token = bin2hex(openssl_random_pseudo_bytes(512));
 
-					// Add Token for User
+						DB::connection('mysql.write')
+						  ->table('login_try')
+						  ->insert([
+							  'username' => $request->input('username'),
+							  'username_hash' => sha1($request->input('username')),
+							  'status' => 'success',
+							  'created_at' => time()
+						  ]);
 
-					return response()->json([
-						'result' => [
-							'status' => 'success',
-							'message' => 'User authenticated.',
-							'token' => $token
-						],
-						'request' => [
-							'username' => $request->input('username')
-						],
-					]);
+						DB::connection('mysql.write')
+						  ->table('auth_tokens')
+						  ->insert([
+							  'token' => $token,
+							  'UID' => $user->id,
+							  'created_at' => time()
+						  ]);
+
+						// Extend Controller from CustomController
+						// Add custom controller as requirement
+						// create module for CustomController
+
+						return response()->json([
+							'result' => [
+								'status' => 'success',
+								'message' => 'User authenticated.',
+								'token' => $token
+							],
+							'request' => [
+								'username' => $request->input('username')
+							],
+						]);
+					}
+					else{
+						DB::connection('mysql.write')
+						  ->table('login_try')
+						  ->insert([
+							  'username' => $request->input('username'),
+							  'username_hash' => sha1($request->input('username')),
+							  'status' => 'failed',
+							  'created_at' => time()
+						  ]);
+
+						return response()->json([
+							'result' => [
+								'status' => 'warning',
+								'message' => 'User credentials wrong.'
+							]
+						]);
+					}
 				}
-				else{
-					return response()->json([
-						'result' => [
-							'status' => 'warning',
-							'message' => 'User credentials wrong.',
-							'token' => $token
-						]
-					]);
-				}
+
+				return response()->json([
+					'result' => [
+						'status' => 'error',
+						'message' => 'User doesnt exists.'
+					]
+				]);
 			}
+			else{
+				DB::connection('mysql.write')
+				  ->table('login_try')
+				  ->insert([
+					  'username' => $request->input('username'),
+					  'username_hash' => sha1($request->input('username')),
+					  'status' => 'blocked',
+					  'created_at' => time()
+				  ]);
 
-			return response()->json([
-				'result' => [
-					'status' => 'error',
-					'message' => 'User doesnt exists.'
-				]
-			]);
+				return response()->json([
+					'result' => [
+						'status' => 'error',
+						'message' => 'User login blocked.'
+					]
+				]);
+			}
 		}
 	}
