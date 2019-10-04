75e6d4ca1c02e5ef51b79d79b0b7809b3bf72d32
jonathan <bitbucket@jmartz.de>
2019-10-04 11:25:57 +0200

    add migrations for auto token and login try table

---
+++ migrations/login-try-table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLoginTryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('login_try', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('username_hash');
+            $table->string('status');
+            $table->integer('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('login_try');
+    }
+}
