b39dad3d84e83f985bc4b6fd628c2bddb7335588
jonathan <bitbucket@jmartz.de>
2019-10-28 19:24:10 +0100

    fix migrations name

--- migrations/2019_10_28_175139_create_users_table.php
+++ migrations/2019_10_28_175139_create_users_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
-class CreateUsersTable extends Migration
+class CreateUserTable extends Migration
 {
     /**
      * Run the migrations.
